6777714f067be1c8fcbb465ef65a5218bf42f00f
ThivyanthyDev <devdhivy@gmail.com>
2021-01-08 21:27:08 +0530

    Add project files.

---
+++ Template Method/TestTemplateMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Method_Pattern.Template_Method
+{
+    class TestTemplateMethod
+    {
+        static void Main()
+        {
+            ExcelFile obj = new ExcelFile();
+            obj.ReadProcessandSave();
+            //obj.ReadData();
+            //obj.ProcessData();
+            //obj.SaveData();
+
+            TextFile obj2 = new TextFile();
+            obj2.ReadProcessandSave();
+            //obj2.ReadData();
+            //obj2.ProcessData();
+            //obj2.SaveData();
+
+        }
+    }
+}
---
+++ Template Method/TextFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Method_Pattern.Template_Method
+{
+    public class TextFile : DataProcessor
+    {
+
+        public override void ReadData()
+        {
+            Console.WriteLine("Read data from Text file");
+        }
+        public override void ProcessData()
+        {
+            Console.WriteLine("Process data from Text file");
+        }
+
+    }
+}
